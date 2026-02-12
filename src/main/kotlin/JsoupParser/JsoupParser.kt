47249ec0859ae848c4e7406ecf5db2c38a397b7d
Андрей <minombre.andrei@yandex.ru>
2026-02-12 22:06:56 +0400

    KS-5-8 JsoupParser

---
+++ src/main/kotlin/JsoupParser/JsoupParser.kt
@@ -0,0 +1,15 @@
+package org.example.JsoupParser
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
+fun main() {
+    val doc: Document =
+        Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/")
+            .get()
+    println(doc.title())
+
+    val quotes: Elements = doc.select(".sc-2aegk7-2.bzpNIu")
+    println(quotes)
+}
